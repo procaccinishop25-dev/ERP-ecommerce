cb27cca544aba06e3c9cc96cd7f8b1a86c62f5fc
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-05-01 15:56:16 +0200

    Create marketplaces.py

---
+++ config/marketplaces.py
@@ -0,0 +1,12 @@
+MARKETPLACES = {
+    "Temu": {
+        "columns": {
+            "order_id": "ID Ordine",
+            "date": "Data ordine",
+            "sku": "Codice SKU",
+            "qty": "quantità acquistata",
+            "price": "prezzo base della merce",
+            "total": "fatturato totale ordine"
+        }
+    }
+}
