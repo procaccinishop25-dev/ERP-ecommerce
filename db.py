88f2f05a99b849e85abb8b41f1ab69f313827777
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-05-01 15:24:19 +0200

    Delete db.py

--- db.py
+++
@@ -1,8 +0,0 @@
-import streamlit as st
-from supabase import create_client
-
-def get_supabase():
-    return create_client(
-        st.secrets["supabase"]["url"],
-        st.secrets["supabase"]["key"]
-    )
