a2e0170ab950e34a9e4c6b2fd2144649a51a99b4
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-05-01 13:28:59 +0200

    Create temu.py

---
+++ parsers/temu.py
@@ -0,0 +1,13 @@
+def parse_temu(df):
+
+    df = df.rename(columns={
+        "ID Ordine": "ordine_id",
+        "Codice SKU": "sku",
+        "quantità acquistata": "quantita",
+        "Totale prezzo base dopo lo sconto": "prezzo_base",
+        "Totale spedizione": "spedizione",
+        "Imposta sull'articolo": "imposta_articolo",
+        "Imposta sulla spedizione": "imposta_spedizione"
+    })
+
+    return df
