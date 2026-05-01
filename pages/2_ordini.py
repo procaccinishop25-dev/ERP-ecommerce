eb5541032e1a14cc724bba3fe6237ccb647bbef2
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-05-01 15:24:03 +0200

    Delete pages directory

--- pages/2_ordini.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-from db import supabase
-
-def show():
-    st.title("Ordini")
-
-    res = supabase.table("ordini").select("*").execute()
-
-    st.dataframe(res.data)
--- pages/dashboard.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show():
-    st.title("Dashboard")
-    st.info("KPI in arrivo...")
