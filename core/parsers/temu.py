38c33622899cdd3f770edb124fb9ffe1de9ead2f
procaccinishop25-dev <procaccinishop25@gmail.com>
2026-05-02 11:25:53 +0200

    Create temu.py

---
+++ core/parsers/temu.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+def parse(file):
+
+    df = pd.read_excel(file)
+
+    orders = {}
+
+    for _, row in df.iterrows():
+
+        order_id = row["ID Ordine"]
+
+        if order_id not in orders:
+            orders[order_id] = {
+                "order_id": order_id,
+                "country": row["Paese di spedizione"],
+                "items": []
+            }
+
+        orders[order_id]["items"].append({
+            "sku": row["Codice SKU"],
+            "qty": int(row["quantità acquistata"]),
+            "revenue": 0
+        })
+
+    return orders
